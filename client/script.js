ff80c25b4002e927c222c86ec05dde7facb02443
Mohit Kumar Gupta <49000628+macck7@users.noreply.github.com>
2022-12-30 12:13:41 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -97,7 +97,7 @@ const handleSubmit = async (e) => {
 
 
   // fetch data from server -> bot's reponse
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://openai-kdmd.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
